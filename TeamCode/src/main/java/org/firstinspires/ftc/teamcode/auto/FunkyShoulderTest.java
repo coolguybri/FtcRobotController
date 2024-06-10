dc81d4d850c40680db60299b92698008222aa8d0
Brian <brian@connolly.net>
2024-06-10 15:47:34 -0700

    Update FunkyShoulderTest.java
    From Maddy and Easton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/FunkyShoulderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/FunkyShoulderTest.java
@@ -10,19 +10,38 @@ public class    FunkyShoulderTest extends AutoDriveTest {
     @Override
     public void ratCrewGo() {
         //moveArmDown(2000);
-        moveArmCounter(5000);
+        encoderDrive(20);
+        wristSet(0.55F);
+        ratCrewWaitMillis(1000);
+        moveArmCounter(5300);
+
+
 
         //moveArmCounter(1000);
-
-
-        ratCrewWaitMillis(500);
         openFinger();
-        ratCrewWaitMillis(500);
+        ratCrewWaitMillis(1000);
         closeFinger();
-
+        ratCrewWaitMillis(1500);
         moveArmCounter(-1000);
+        ratCrewWaitMillis(2000);
 
+        encoderDrive(36);
+        ratCrewWaitMillis(2000);
+        turnRight(80);
+        ratCrewWaitMillis(2000);
+        encoderDrive(67);
+        ratCrewWaitMillis(2000);
+        turnRight(80);
 
+        ratCrewWaitMillis(2000);
+        encoderDrive(18);
+        ratCrewWaitMillis(2000);
+        turnLeft(80);
+        ratCrewWaitMillis(2000);
+        encoderDrive(10);
+        ratCrewWaitMillis(2000);
+        moveArmCounter(-1000);
+        openFinger();
         // test change.
     }
 }
