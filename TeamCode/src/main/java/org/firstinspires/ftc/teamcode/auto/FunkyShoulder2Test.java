abd50d9fcc7fb51b4bf961b3e1c3412fcf1fe274
Brian <brian@connolly.net>
2024-06-10 16:11:13 -0700

    Create FunkyShoulder2Test.java
    update from Sasha and Nova

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/FunkyShoulder2Test.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ */
+@Autonomous(name="ARM Goofy", group="AA DanceDemo")
+public class    FunkyShoulder2Test extends AutoDriveTest {
+
+    @Override
+    public void ratCrewGo() {
+        //moveArmDown(2000);
+
+        // attempt1
+        moveArmCounter(2500);
+        wristSet(0.5f);
+        openFinger();
+        ratCrewWaitMillis(1000);
+
+       // moveArmCounter(3000);
+        moveArmCounter(2150);
+
+
+        ratCrewWaitMillis(500);
+       // openFinger();
+        ratCrewWaitMillis(500);
+        closeFinger();
+        ratCrewWaitMillis(1000);
+
+        // attempt1
+        moveArmCounter(-450);
+        wristSet(0.6f);
+        moveArmCounter(-450);
+        //moveArmCounter(1000);
+
+
+
+        // New code!
+       // ratCrewWaitMillis(2000);
+        //wristSet(0.5f);
+      //  ratCrewWaitMillis(2000);
+       // wristSet(1.0f);
+
+        //moveArmCounter(-1000);
+
+
+        // test change.
+    }
+}
